8af5983d113ae0538dcbc18f4269a8ecd8abd5d5
s-fairchild <steve51516@gmail.com>
2026-01-18 06:36:11 -0500

    Remove TTY input/output tests
    When these return an exit code != 0, set -o errexit causes the script to fail.
    These fail in my bash scripts. I'm unsure why, but it works fine without this check.

--- util/base.sh
+++ util/base.sh
@@ -24,12 +24,6 @@ SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 ROOT_PID=$$
 PARENT_PID=$PPID
 
-# get input/output redirection state
-[[ ! -t 0 ]]; IS_STDIN_TTY="${?}"
-[[ ! -t 1 ]]; IS_STDOUT_TTY="${?}"
-[[ ! -t 2 ]]; IS_STDERR_TTY="${?}"
-[[ ! "$IS_STDIN_TTY$IS_STDOUT_TTY$IS_STDERR_TTY" == "111" ]]; IS_TTY="${?}"
-
 ### General Helpers
 
 # Return the assosciative array declaration without the `declare -A name=` part
